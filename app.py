163e0ab45f71774db5bb6b03e067e9ee817cfc2d
Andy <jihyuk.sung@gmail.com>
2021-05-08 18:26:50 -0600

    Setup Complete

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, session, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "test123"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
+
+
+@app.route("/")
+def render_home():
+    return "Hello"
